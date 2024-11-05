0a2fedbca44304e8c7c05bfc38bbaabed5c2cb96
Guilherme Torres Castro <1149991+guitcastro@users.noreply.github.com>
2024-11-05 17:40:13 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,8 @@ mkdir buid
 mkdir install
 cd buid
 
+brew install ninja
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS='lld' -DCMAKE_INSTALL_PREFIX=./install ../llvm
+
+make lld
+make install
