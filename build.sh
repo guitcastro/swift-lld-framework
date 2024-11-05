b2a7b9efdc11a16a9f6821120ba40ef7903d7ac3
Guilherme Torres Castro <1149991+guitcastro@users.noreply.github.com>
2024-11-05 17:31:32 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+curl -sL https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-19.1.3.zip -o llvm-project.zip
+unzip llvm-project.zip
+
+cd llvm-project-llvmorg-19.1.3/
+
+mkdir buid
+mkdir install
+cd buid
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS='lld' -DCMAKE_INSTALL_PREFIX=./install ../llvm
